8767c645bba37dd70a1f19f222b5dad31fa416a5
Umme Aiman <122363026+umme-aimany@users.noreply.github.com>
2024-01-03 23:14:57 +0500

    Add files via upload

---
+++ PYTHON-FOR-LOOP/index.py
@@ -0,0 +1,14 @@
+
+number=1
+ while (number <= 10):
+    print("hellow world")
+   number = number + 2
+
+
+
+
+# in this case loop is infinite
+# number=1
+# while (number != 10):
+#     print(number)
+#     number = number + 2
